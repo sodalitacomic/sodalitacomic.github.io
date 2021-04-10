712b7ad03c004e3223419e551c4751d315c98ce7
Beatriz Oliveira <beafoliveira@gmail.com>
2021-04-10 18:00:02 -0300

    mudança de rumo

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# Heading
+[![acessar webcomic](/img/header.png)](https://www.google.com "acessar site da webcomic")
 
-[LINK AQUI](https://www.google.com)
+# Sodalita: A Jornada de Rui na Caverna de Cristal
 
-![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-```javascript
-var s = "JavaScript syntax highlighting";
-alert(s);
-```
+[※ Acessar WebComic](https://www.google.com)
 
-| Tables        |      Are      |  Cool |
-| ------------- | :-----------: | ----: |
-| col 3 is      | right-aligned | $1600 |
-| col 2 is      |   centered    |   $12 |
-| zebra stripes |   are neat    |    $1 |
-
-git push -u origin main
-git pull origin main
+[//]: <> (git push -u origin main)
+[//]: <> (git pull origin main)
--- js/index.js
+++ js/index.js
@@ -1,44 +1,11 @@
 window.onkeydown = teclaPressionada;
 window.onkeyup = teclaSolta;
-var contexto;
-var canvas;
+
 var pagina = 0;
 var quadro = 0;
 var pressEsq = false;
 var pressDir = false;
 var mapaTecla = new Array(); // para otimizar o reconhecimento de tecla pressionada
-
-var des1 = document.getElementById("f1");
-var des2 = document.getElementById("f2");
-var des3 = document.getElementById("f3");
-var des4 = document.getElementById("f4");
-var des5 = document.getElementById("f5");
-var des6 = document.getElementById("f6");
-var des7 = document.getElementById("f7");
-
-var x1 = 200;
-var y1 = 50;
-var larx1 = 500;
-var alt1 = 489;
-
-
-
-function pintar() {
-    //contexto.clearRect(0, 0, 500, 600);
-
-    //contexto.font = "22pt Arial";
-    //contexto.fillStyle = "black";
-    switch (pagina) { //timeline - trocar do prologo para outras paginas
-        case 0: desenharHQ();
-            break;
-            // case 1: contexto.fillText("Em breve1", 250, 250);
-            break;
-            // case 2: contexto.fillText("Em breve2", 250, 250);
-            break;
-            // case 3: contexto.fillText("Em breve3", 250, 250);
-            break;
-    }
-}
 
 function teclaPressionada(evento) {
     // padrão ABNT2:a tecla da seta para esquerda tem valor 37 e a seta para direita tem o valor 39
@@ -51,15 +18,10 @@ function teclaSolta(evento) {
 }
 
 function iniciar() {
-    //canvas = document.getElementById("hqCanvas");
-    //contexto = canvas.getContext("2d");
-
     temporizador = setInterval(mainloop, 33);
 }
 
 function mainloop() {
-    pintar();
-
     if (mapaTecla[39] == true && quadro < 3) {
         if (!pressDir) {
             pressDir = true; //impede que passe vários quadros ao pressionar a seta direita
@@ -85,10 +47,6 @@ function mainloop() {
     if (mapaTecla[37] == false) {
         pressEsq = false; //permite passar outro quadro ao clicar na seta esquerda
     }
-}
-
-function desenharHQ() {
-
 }
 
 function proximo() {
