eac53eb39e4e233805a5b2f71c87d991504bc2fc
Beatriz Oliveira <beafoliveira@gmail.com>
2021-06-18 00:47:04 -0300

    ui adjustments

--- js/audio.js
+++ js/audio.js
@@ -1,4 +1,15 @@
+var audio = new Audio("/audio/soundtrack/roxo.wav");
+
+var isPlaying = false;
+
 function playAudio() {
-  var audio = new Audio("/audio/p1_1.wav");
-  audio.play();
+  isPlaying ? audio.pause() : audio.play(); 
 }
+
+audio.onplaying = function() {
+  isPlaying = true;
+};
+
+audio.onpause = function() {
+  isPlaying = false;
+};
--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,65 @@
-function goTop() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function prevPage() {
+  // get site url
+  var pageURL = window.location.href.toString();
+  // main website url without page html
+  var page = pageURL.substring(0, pageURL.lastIndexOf("/") + 1);
+  // page number without html extension
+  var pageURLNum = pageURL.substring(pageURL.length - 7, pageURL.length - 5);
+  // page number as integer
+  var pageNum = parseInt(pageURLNum, 10);
+
+  var prevPage = pageNum - 1;
+
+  if (prevPage < 10) {
+    var previousPage = "0" + prevPage;
+  } else if (prevPage >= 10)
+    var previousPage = prevPage;
+  
+  if (prevPage == 1) {
+    window.location.href = page + "index.html";
+  } else {
+    window.location.href = page + "pg" + previousPage + ".html";
+  }
+
+  if (pageURLNum == "ex") {
+    window.location.href = page + "index.html";
+  } else {
+    window.location.href = page + "pg" + nexxtPage + ".html";
+  }
+}
+
+function nextPage() {
+  // get site url
+  var pageURL = window.location.href.toString();
+  // main website url without page html
+  var page = pageURL.substring(0, pageURL.lastIndexOf("/") + 1);
+  // page number without html extension
+  var pageURLNum = pageURL.substring(pageURL.length - 7, pageURL.length - 5);
+  // page number as integer
+  var pageNum = parseInt(pageURLNum, 10);
+
+  var nextPage = pageNum + 1;
+
+  if (nextPage < 10) {
+    var nexxtPage = "0" + nextPage;
+  } else if (nextPage >= 10) {
+    var nexxtPage = nextPage;
+  }  
+
+  if (pageURLNum == "ex") {
+    window.location.href = page + "pg02.html";
+  } else if (pageURLNum == "16") {
+    window.location.href = page + "index.html";
+  } else {
+    window.location.href = page + "pg" + nexxtPage + ".html";
+  }
+}
+
+function teamPage() {
+  // get site url
+  var pageURL = window.location.href.toString();
+  // main website url without page html
+  var page = pageURL.substring(0, pageURL.lastIndexOf("/") + 1);
+
+  window.location.href = page + "team.html";
 }
